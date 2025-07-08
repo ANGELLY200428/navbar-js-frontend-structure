c2d865e4ec259db40aaeaac512c294eb11b9146a
Angelly Agudelo <angellyna2004@gmail.com>
2025-07-07 19:04:30 -0500

    feat: Sea agrego el hero, se mejoraron los estilos del navbar

--- frontend/public/js/components/navbar.js
+++ frontend/public/js/components/navbar.js
@@ -5,8 +5,27 @@ document.addEventListener("DOMContentLoaded", function(){
         fetch("/frontend/public/views/components/navbar.html")
         .then(response => response.text())
         .then(data => {navbarElement.innerHTML = data;
+            
+        //===Lógica para resaltar el enlace que está activo en el navbar
+
+        //Obtener la ruta actual
+        //Si no hay un archivo específico se asume que es el index
+        const currentPage = window.location.pathname.split("/").pop() || "index.html";
+
+        //Selecciona todos los enlaces del navbar que usaran la clase personalizada
+        const navbarLinks = navbarElement.querySelectorAll(".navbar__link");
+
+        //Recorre cada enlace del navbar
+        navbarLinks.forEach(link => {
+
+            //Verifica si el href del enlace incluye el nombre de la página actual
+            if(link.getAttribute("href").includes(currentPage)){
+                //Si es la página actual se le asigna la clase 'active' para destacarla visualmente
+                link.classList.add("active");
+            }
+        });
     })
-    .catch(error => console.error("Error cargando el navbar"))
+    .catch(error => console.error("Error cargando el navbar"));
 
     }
 }); 
