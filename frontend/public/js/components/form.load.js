9c16c9ad8d0558443307ca31210e643cdc571d2e
Angelly Agudelo <angellyna2004@gmail.com>
2025-09-09 15:31:05 -0500

    feat: Se creo formulario de proyecto Freelancer

--- frontend/public/js/components/form.load.js
+++ frontend/public/js/components/form.load.js
@@ -1,18 +1,16 @@
 // form/form.load.js
 
-const { append } = require("vary");
-
 document.addEventListener("DOMContentLoaded", async () =>{
     const container = document.querySelector(".services__form-container");
     if (container){
-        const response = await fetch("/frontend/public/views/form.html")
+        const response = await fetch("/frontend/public/views/components/form.html")
         const html = await response.text();
         container.innerHTML = html;
 
         
     // Carga la lógica del formulario
     const script = document.createElement('script');
-    script.src = "/frontend/public/js/components/form.logic.js";
+    script.src = "/frontend/public/js/components/form.logic.js"
     document.body.appendChild(script);
     }
 });
--- frontend/public/js/components/form.logic.js
+++ frontend/public/js/components/form.logic.js
@@ -1,3 +1,41 @@
+// Lógica del formulario de contacto de proyecto Frelancer
+
+// El objetivo es poder manipular los datos que se registren en el formulario
+// Explicación a fondo de las siguientes dos líneas de código
+    //=== const formdata = new FormData(form);
+    //=== const data =Object.formEntries(formdata);
+
+// ¿Qué significa new?
+// En JavaScript, la palabra reservada new sirve para crear una nueva instancia de un objeto a partir de una función constructora o una clase.
+// FormData(form) = [
+//     ["Nombre", "Angelly Agudelo"]
+//     ["Teléfono", "3054411190"]
+//     ["Correo electrónico", "angellna2004@gmail.com"]
+//     ["Mensaje", "Hola que tal?, requiero información"]
+// ]
+
+// // Ahora transformo esos datos en un objeto JS, con esta li´nea de código data = Object.fromEntries(formdata);
+// data = {
+//     Nombre: "Angelly Agudelo",
+//     Teléfono: "3054411190",
+//     Correo: "angellna2004@gmail.com",
+//     Mensaje: "Hola que tal?, requiero información"
+// }
+
+// // Por último convierto ese objeto JS  a JSON
+
+// {
+//     Nombre: "Angelly Agudelo",
+//     Teléfono: "3054411190",
+//     Correo: "angellna2004@gmail.com",
+//     Mensaje: "Hola que tal?, requiero información"
+// }
+
+
+
+
+//const { FormData } = require("undici-types");
+
 // Espera que el DOM este completamente cargado antes de ejecutar el script
 document.addEventListener('DOMContentLoaded', () =>{
     // Esto selecciona el formulario con la clase contact-form__form 
@@ -16,9 +54,25 @@ document.addEventListener('DOMContentLoaded', () =>{
 
             try{
 
-                const response
-            }
-        })
+                const response = await fetch("/api/contact", {
+                    method: "POST",
+                    headers: {"Content-Type":    "application/json"},
+                    // Covierte el objeto de los datos del formulario a formato JSON
+                    body: JSON.stringify(data),      
+                });
 
-    }
+                // Verifica si la respuesta es éxitosa (codigo 200 - 299)
+                if(response.ok) {
+                    alert("Mensae enviado con éxito");
+                    form.reset();
+                }else {
+                    alert("Hubo un problema al enviar el mensaje")//Notifica de un error en el servidor
+                }
+            } catch (error) {
+                console.error(error);
+                alert("Error de conexión")
+            }
+        });
+
+    };
 });
